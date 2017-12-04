a8a90334ebccca5b48a4f0eb28a9ed1d2dc72818
John Geliberte <gelibertejohn@gmail.com>
2017-12-04 10:46:29 +0800

    Finalize login page

--- codeigniter/application/views/home_page.php
+++ codeigniter/application/views/home_page.php
@@ -1,4 +1,3 @@
-<script type="text/javascript" src="/js/anonymsgr/matrix_bg.js"></script>
 <link href="/css/anonymsgr/matrix_bg.css" rel="stylesheet">
 <link href="/css/anonymsgr/home.css" rel="stylesheet">
 
@@ -11,9 +10,11 @@
 				<label for="">LOGIN:</label>
 				<input class="form-control" type="text">
 				<label for="">HASH CODE:</label>
-				<input class="form-control" type="text">
+				<input class="form-control" type="password">
 			</div>
 		</div>
 		<div class="col-sm-4"></div>
 	</div>
 </div>
+
+<script type="text/javascript" src="/js/anonymsgr/matrix_bg.js"></script>
